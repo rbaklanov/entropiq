c3912c694df453669991eb28207428496abedd83
rbaklanov <rbaklanov@gmail.com>
2026-03-03 13:14:57 +0300

    ENQ-19 Create guest layout and landing page
    - fixed the issue of pest testing failed;

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -10,7 +10,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
     @fluxAppearance
 </head>
 <body class="min-h-screen bg-white font-sans text-gray-900 antialiased">
